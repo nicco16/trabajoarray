25ea918048ab41860ea2da63321cfabe6ab024c2
nicco16 <ncamiletti4@gmail.com>
2023-08-21 12:21:09 -0300

    trabajo terminado

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Array extranio
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+]
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+const sortedStrings = filterAndSortStrings(strangeArray);
+function filterAndSortStrings(array) {
+  const strings = array.filter(item => typeof item === 'string');
+  return strings.sort();
+}
+
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  showList(sortedStrings);
+});
